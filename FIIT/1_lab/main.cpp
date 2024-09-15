d04b6451aaf96bf517fdec236b3894665f50e41d
chponya <chponyaaa@gmail.com>
2024-09-15 17:34:05 +0200

    Add: Создана папка для лаб работы

---
+++ FIIT/1_lab/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>     // директива препроцессора
+using namespace std;    // директива using
+int main()              // функция с именем main
+{                       // начало тела функции
+    cout << "Hello, everynyan ^^!";  // оператор
+    return 0;           // оператор
+}                       // конец тела функции

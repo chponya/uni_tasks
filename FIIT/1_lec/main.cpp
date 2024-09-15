bf8cfd4e13c239acdae1b3221b8c9bd39cc83268
chponya <chponyaaa@gmail.com>
2024-09-15 13:23:34 +0200

    Add: Добавлены комментарии

--- FIIT/1_lec/main.cpp
+++ FIIT/1_lec/main.cpp
@@ -1,7 +1,7 @@
-#include <iostream>
-using namespace std;
-int main()
-{
-    cout << "Hello, everynyan ^^!";
-    return 0;
-}
+#include <iostream>     // директива препроцессора
+using namespace std;    // директива using
+int main()              // функция с именем main
+{                       // начало тела функции
+    cout << "Hello, everynyan ^^!";  // оператор
+    return 0;           // оператор
+}                       // конец тела функции
